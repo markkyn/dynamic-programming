5309d74f12a4dca85fd7d3c7b9f2209b7d7ea5c9
markkyn <marcos.gabriel@ossirian.com>
2024-02-04 01:34:34 -0300

    refactor

--- knapsack-problem/mochila.c
+++ knapsack-problem/mochila.c
@@ -36,6 +36,41 @@ int max(int a, int b)
     return b;
 }
 
+void calc_value_mt(Matrix_t *matrix, Item *items)
+{
+    // setting the first row
+    for (int w = 0; w < matrix->cols; w++)
+    {
+        set_value(matrix, 0, w, 0);
+    }
+
+    // setting the first col
+    for (int i = 0; i < matrix->rows; i++)
+        set_value(matrix, i, 0, 0);
+
+    // where N is the number of items
+    for (int i = 1; i < matrix->rows; i++)
+    {
+        // where W is the max capacity of the sack
+        for (int w = 1; w < matrix->cols; w++)
+        {
+            if (w < items[i - 1].weight)
+            {
+                int x = get_value(matrix, i - 1, w);
+                set_value(matrix, i, w, x);
+            }
+            else
+            { // Fits
+                int32_t wout_item = get_value(matrix, i - 1, w - items[i - 1].weight) + items[i - 1].value;
+                int32_t with_item = get_value(matrix, i - 1, w);
+
+                int m = max(with_item, wout_item);
+                set_value(matrix, i, w, m);
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     if (argc < 3)
@@ -67,44 +102,15 @@ int main(int argc, char *argv[])
         .arr_ptr = (int32_t *)malloc((C + 1) * (N + 1) * sizeof(int32_t)),
     };
 
-    // setting the first row
-    for (int w = 0; w < C; w++)
-    {
-        set_value(&V_i_w, 0, w, 0);
-    }
+    calc_value_mt(&V_i_w, items);
 
-    // setting the first col
-    for (int i = 0; i < N; i++)
-        set_value(&V_i_w, i, 0, 0);
-
-    // where N is the number of items
-    for (int i = 1; i < V_i_w.rows; i++)
+    for (int i = 0; i < V_i_w.rows; i++)
     {
-        // where W is the max capacity of the sack
-        for (int w = 1; w < V_i_w.cols; w++)
+        for (int j = 0; j < V_i_w.cols; j++)
         {
-            if (w < items[i-1].weight)
-            {
-                int x = get_value(&V_i_w, i - 1, w);
-                set_value(&V_i_w, i, w, x);
-            }
-            else
-            { // Fits
-                int32_t wout_item = get_value(&V_i_w, i - 1, w - items[i-1].weight) + items[i-1].value;
-                int32_t with_item = get_value(&V_i_w, i - 1, w);
-
-                int m = max(with_item, wout_item);
-                set_value(&V_i_w, i, w, m);
-            }
-        }
-    }
-
-    for(int i  = 0; i < V_i_w.rows; i++){
-        for(int j = 0; j < V_i_w.cols; j++){
             int x = get_value(&V_i_w, i, j);
-            printf("%*d ", 3 , x);
+            printf("%*d ", 3, x);
         }
         printf("\n");
     }
-
 }
